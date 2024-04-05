906e234866cb86a64058b256cc0c8e60668e0f92
jacek <202203150323@zjut.edu.cn>
2024-04-05 15:38:50 +0800

    Using wslpath instead of implement myself

--- src/main.rs
+++ src/main.rs
@@ -10,32 +10,18 @@ fn main() -> io::Result<()> {
     }
 
     // 获取linux发行版
-    let linux_distro = args[1].clone();
+    let linux_distro = &args[1];
     // 获取命令
-    let command = args[2].clone();
+    let command = &args[2];
     // 获取文件路径
-    let mut file_path = args[3].clone();
-    // 将路径中的\替换为/，C:替换为/mnt/c，D:替换为/mnt/d，以此类推
-    file_path = file_path.replace("\\", "/");
-    // 如果路径以字母加:开头，说明是Windows绝对路径，需要替换
-    if file_path.chars().nth(1) == Some(':') {
-        let drive_letter = file_path.chars().nth(0).unwrap();
-        // 判断是否是字母
-        if drive_letter.is_ascii_alphabetic() {
-            file_path = file_path.replacen(&format!("{}:", drive_letter), &format!("/mnt/{}", drive_letter), 1);
-        }
-    }
-
-    // println!("linux_distro: {}", linux_distro);
-    // println!("command: {}", command);
-    // println!("file_path: {}", file_path);
+    let file_path = &args[3];
 
     // 执行命令 wsl -d <linux_distro> <command> <file_path>
     let output = Command::new("wsl")
         .arg("-d")
-        .arg(&linux_distro)
-        .arg(&command)
-        .arg(&file_path)
+        .arg(linux_distro)
+        .arg(command)
+        .arg(&format!("`wslpath -au '{}'`", file_path))
         .output()
         .expect("Failed to execute command");
 
